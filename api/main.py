15d89f2423cf0b9a95c4c9e572885b2e84e9db81
Alam <malam1984@gmail.com>
2025-08-24 22:16:18 +0530

    checking deployment

--- api/main.py
+++ api/main.py
@@ -38,7 +38,7 @@ app.add_middleware(
 
 @app.get("/", response_class=HTMLResponse)
 async def serve_ui(request: Request):
-    log.info("Serving UI homepage checked.")
+    log.info("Serving UI homepage.")
     resp = templates.TemplateResponse("index.html", {"request": request})
     resp.headers["Cache-Control"] = "no-store"
     return resp
